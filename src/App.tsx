542469f7a098ed5cea494dff9c9d3374ba60a8ce
Bukola Omojowo <bomojowo@gmail.com>
2021-06-29 13:03:23 +0100

    PAssed Emoji Picker component through to app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import NumberPicker from "./components/NumberPicker";
+import EmojiPicker from "./components/EmojiPicker";
 
 function App(): JSX.Element {
-  return <NumberPicker />;
+  return( 
+  <>
+  <NumberPicker />
+  <hr />
+  <EmojiPicker />
+  </>
+  )
+  
 }
 
 export default App;
